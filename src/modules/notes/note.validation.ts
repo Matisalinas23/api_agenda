0f9022f7462bd145caec0aa7732449d30c068c33
Matias Salinas <matisalinas1602@gmail.com>
2026-01-16 19:16:32 -0300

    type_and_length_validations_in_notes_module

--- src/modules/notes/note.validation.ts
+++ src/modules/notes/note.validation.ts
@@ -5,22 +5,44 @@ export function validateCreateNote(dto: ICreateNote): void {
   if (!dto.title) {
     throw new ValidationError("Title is required")
   }
+  if (typeof dto.title !== "string") {
+    throw new ValidationError("Tile must be a string")
+  }
+  if (dto.title.length <= 60) {
+    throw new ValidationError("Tile must have 60 or less characters")
+  }
 
   if (!dto.assignature) {
     throw new ValidationError("Assignature is required")
+  }
+  if (typeof dto.assignature !== "string") {
+    throw new ValidationError("Assignature must be a string")
+  }
+  if (dto.assignature.length <= 30) {
+    throw new ValidationError("Assignature must have 30 or less characters")
   }
 
   if (!dto.color) {
     throw new ValidationError("Color is required")
   }
+  if (typeof dto.color !== "string") {
+    throw new ValidationError("Color must be a string")
+  }
+  if (dto.color.length <= 10) {
+    throw new ValidationError("Color must have 10 or less characters")
+  }
+  
 
   if (!dto.limitDate) {
     throw new ValidationError("LimitDate is required")
   }
+  if (typeof dto.limitDate !== "string") {
+    throw new ValidationError("LimitDate must be a string")
+  }
 
   const date = new Date(dto.limitDate)
 
-  if (!isNaN(date.getTime())) {
+  if (isNaN(date.getTime())) {
     throw new ValidationError("LimitDate has an invalid format")
   }
 }
