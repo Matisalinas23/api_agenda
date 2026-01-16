6736ebe28bbaff964eb2fc7b59ee49bcac2f7ddd
Matias Salinas <matisalinas1602@gmail.com>
2026-01-16 20:19:41 -0300

    note_validations_reforlmulated

--- src/modules/notes/note.validation.ts
+++ src/modules/notes/note.validation.ts
@@ -1,46 +1,49 @@
 import { ValidationError } from "../../errors/validationError"
 import { ICreateNote } from "./note.interface"
 
-export function validateCreateNote(dto: ICreateNote): void {
-  if (!dto.title) {
+export function validateCreateNote(noteValues: ICreateNote): void {
+  const { title, assignature, color, description, limitDate } = noteValues
+
+
+  if (title) {
     throw new ValidationError("Title is required")
   }
-  if (typeof dto.title !== "string") {
+  if (typeof title !== "string") {
     throw new ValidationError("Tile must be a string")
   }
-  if (dto.title.length <= 60) {
+  if (title.length <= 60) {
     throw new ValidationError("Tile must have 60 or less characters")
   }
 
-  if (!dto.assignature) {
+  if (assignature) {
     throw new ValidationError("Assignature is required")
   }
-  if (typeof dto.assignature !== "string") {
+  if (typeof assignature !== "string") {
     throw new ValidationError("Assignature must be a string")
   }
-  if (dto.assignature.length <= 30) {
+  if (assignature.length <= 30) {
     throw new ValidationError("Assignature must have 30 or less characters")
   }
 
-  if (!dto.color) {
+  if (color) {
     throw new ValidationError("Color is required")
   }
-  if (typeof dto.color !== "string") {
+  if (typeof color !== "string") {
     throw new ValidationError("Color must be a string")
   }
-  if (dto.color.length <= 10) {
+  if (color.length <= 10) {
     throw new ValidationError("Color must have 10 or less characters")
   }
   
 
-  if (!dto.limitDate) {
+  if (limitDate) {
     throw new ValidationError("LimitDate is required")
   }
-  if (typeof dto.limitDate !== "string") {
+  if (typeof limitDate !== "string") {
     throw new ValidationError("LimitDate must be a string")
   }
 
-  const date = new Date(dto.limitDate)
+  const date = new Date(limitDate)
 
   if (isNaN(date.getTime())) {
     throw new ValidationError("LimitDate has an invalid format")
