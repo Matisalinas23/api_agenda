71f15eed0659a354eb045da6440df8b94dea7b67
Matias Salinas <matisalinas1602@gmail.com>
2026-01-19 20:29:22 -0300

    auth_middleware_implementation

--- src/modules/notes/note.router.ts
+++ src/modules/notes/note.router.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import { getAllNotes, createNote, updateNote, deleteNote } from './note.controller'
+import { authMiddleware } from '../../middleweres/authMiddleware'
 
 const router = express.Router()
 
-router.put('/:id', updateNote)
-router.delete('/:id', deleteNote)
-router.get('/', getAllNotes)
-router.post('/', createNote)
+router.put('/:id', authMiddleware, updateNote)
+router.delete('/:id', authMiddleware, deleteNote)
+router.get('/', authMiddleware, getAllNotes)
+router.post('/', authMiddleware, createNote)
 
 export default router
