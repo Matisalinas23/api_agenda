a7f75885530fc64cddc3604419358e61517ae91b
Matias Salinas <matisalinas1602@gmail.com>
2026-01-28 14:34:05 -0300

    generate token and refresh token services

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,39 @@
 import { ConflictError } from "../../errors/conflictError";
 import { ConnectionError } from "../../errors/connectionError";
 import { CustomError } from "../../errors/customError";
+import { notFoundError } from "../../errors/notFoundError";
 import { UnauthorizedError } from "../../errors/unauthorizedError";
 import { prisma } from "../../lib/prisma";
 import { ICreateUser } from "../user/user.interface";
 import { ILogin } from "./auth.interface";
 import { validateLoginUser, validatePassword, validateRegisterUser } from "./auth.validation";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const hashPassword = async (password: string) => {
     return await bcrypt.hash(password, 10)
+}
+
+const generateAccessToken = (userId: number, email: string, SECRET: string) => {
+    return jwt.sign(
+        {
+            userId,
+            email
+        },
+        SECRET,
+        { "expiresIn": "5m" }
+    );
+}
+
+const generateRefreshToken = (userId: number, email: string, REFRESH_SECRET: string) => {
+    return jwt.sign(
+        {
+            userId,
+            email
+        },
+        REFRESH_SECRET,
+        { "expiresIn": "1d" }
+    );
 }
 
 export const registerUserService = async (user: ICreateUser) => {
@@ -49,7 +73,12 @@ export const loginUserService = async (login: ILogin) => {
 
         await validatePassword(login.password, user.password)
 
-        return user
+        const SECRET = process.env.SECRET
+        const REFRESH_SECRET = process.env.REFRESH_SECRET
+        const token = generateAccessToken(user.id, user.email, SECRET!)
+        const refreshToken = generateRefreshToken(user.id, user.email, REFRESH_SECRET!)
+
+        return { token, refreshToken }
     } catch (error: any) {        
         if (error instanceof CustomError) {
             throw error
@@ -57,4 +86,24 @@ export const loginUserService = async (login: ILogin) => {
 
         throw new ConnectionError("Failed to login user", error)
     }
+}
+
+export const refreshTokenService = async (refreshToken: string) => {
+    const REFRESH_SECRET = process.env.REFRESH_SECRET;
+
+    const payload = jwt.verify(refreshToken, REFRESH_SECRET!) as {
+        userId: number;
+        email: string;
+    };
+
+    const user = await prisma.user.findUnique({
+        where: { id: Number(payload.userId) }
+    });
+
+    if (!user) throw new notFoundError("User not found");
+
+    const SECRET = process.env.SECRET
+    const newAccessToken = generateAccessToken(user.id, user.email, SECRET!);
+
+    return newAccessToken;
 }
