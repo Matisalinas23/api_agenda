d59e892e45e8e385527c36e7a8ccb9084f2eef4c
Matias Salinas <matisalinas1602@gmail.com>
2026-01-28 14:32:15 -0300

    cors configuration

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,18 @@ import notesRoutes from "./modules/notes/note.router"
 import authRoutes from "./modules/auth/auth.router"
 import usersRoutes from "./modules/user/user.route"
 import { errorHandler } from "./middleweres/errorHandler"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}))
+app.use(cookieParser());
 
 app.get("/health", (req: Request, res: Response) => {
     res.status(200).json({ status: "OK" })
