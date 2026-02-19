44e90facca8ff1155b1545f2d837efae212594ef
Matias Salinas <matisalinas1602@gmail.com>
2026-02-19 16:33:00 -0300

    cors configuration

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:4173"],
+    origin: ["http://localhost:5173", "http://localhost:4173", "https://agenda-web-sage.vercel.app"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
 }))
