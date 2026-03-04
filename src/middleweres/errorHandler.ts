da348638b6c7ded3af9f7d39a120cfce41048057
Matias Salinas <matisalinas1602@gmail.com>
2026-03-04 18:47:49 -0300

    testing mode for verification for email

--- src/middleweres/errorHandler.ts
+++ src/middleweres/errorHandler.ts
@@ -2,7 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/customError";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log(err)
+    if (process.env.NODE_ENV !== "test") {
+        console.log(err)
+    }
     
     if (err instanceof CustomError) {
         return res.status(err.statusCode).json({
--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -3,7 +3,8 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-
+    if (process.env.NODE_ENV === "test") return;
+    
     const { error } = await resend.emails.send({
         from: "Agenda <onboarding@resend.dev>",
         to: email,
