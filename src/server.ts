d30d1c6ec1f04ce9fb030c1272a27fa909ee7f54
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-08 19:40:30 -0300

    fixing: force ipv4 from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+import dns from "node:dns"
 import dotenv from "dotenv"
 dotenv.config()
 
 import app from "./app"
 import { initCronJobs } from "./cron"
 
+dns.setDefaultResultOrder("ipv4first");
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`API running on http://localhost:${PORT}`)
     initCronJobs()
 })
+console.log("Node version:", process.version);
