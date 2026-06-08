2f0b5e44960d82ae9d6e52db048c4c1b4645f165
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-08 19:59:43 -0300

    fixing: login default result order

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import app from "./app"
 import { initCronJobs } from "./cron"
 
 dns.setDefaultResultOrder("ipv4first");
+console.log("DNS Order:", dns.getDefaultResultOrder());
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
