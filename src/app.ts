9275f7478e94297673b33d2ae472a130036eea4a
Matisalinas23 <maatisalinas18@gmail.com>
2026-06-09 18:19:14 -0300

    fix: cors configuratin updated

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:4173", "https://agenda-web-olive.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:4173", "https://agenda-web-self.vercel.app"],
     allowedHeaders: ["Content-Type", "Authorization", "x-api-key"],
     credentials: true,
 }))
